4a3ff594ab838faa7803622671547ccd17119d12
vl <vasil.deville@gmail.com>
2017-07-04 07:51:50 -0400

    my nth first commit:(

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone as tz
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=4000)
+    date_created = models.DateTimeField(default=tz.now)
+    date_published = models.DateTimeField(null=True, blank=True)
+
+    def publish(self):
+        date_published=tz.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
